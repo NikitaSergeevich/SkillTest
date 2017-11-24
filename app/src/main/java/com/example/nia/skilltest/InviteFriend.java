2415a06f1ad86f561c7b3153ff887dddc11f23e9
Nikitka <navrelin1993@gmail.com>
2017-11-24 19:37:53 +0300

    First Screen

---
+++ app/src/main/java/com/example/nia/skilltest/InviteFriend.java
@@ -0,0 +1,29 @@
+package com.example.nia.skilltest;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class InviteFriend extends AppCompatActivity {
+
+    ActionBar actionBar;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_friend);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(null);
+        }
+    }
+}
